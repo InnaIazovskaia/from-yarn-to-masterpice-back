9b9a0126e0b0b108899f1ec2ba693c8ca1927573
Inna Iazovskaia <inna.0961@mail.ru>
2023-03-14 11:29:42 +0100

    Create user/register endpoint

--- src/server/routers/userRouter/userRouter.ts
+++ src/server/routers/userRouter/userRouter.ts
@@ -1,17 +1,24 @@
 import express from "express";
 import { validate } from "express-validation";
-import { userLogin } from "../../controllers/userControllers.js";
-import userLoginShema from "../../schemas/userLoginShema.js";
+import { userLogin, userRegister } from "../../controllers/userControllers.js";
+import userLoginSchema from "../../schemas/userLoginSchema.js";
+import userRegisterSchema from "../../schemas/userRegisterSchema.js";
 import paths from "../paths.js";
 
 const userRouter = express.Router();
 
-const { loginPath } = paths;
+const { loginPath, registerPath } = paths;
 
 userRouter.post(
   loginPath,
-  validate(userLoginShema, {}, { abortEarly: false }),
+  validate(userLoginSchema, {}, { abortEarly: false }),
   userLogin
+);
+
+userRouter.post(
+  registerPath,
+  validate(userRegisterSchema, {}, { abortEarly: false }),
+  userRegister
 );
 
 export default userRouter;
